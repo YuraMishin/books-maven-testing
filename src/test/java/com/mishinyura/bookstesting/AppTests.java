0c6b04f4708fd069525f6e09045acd91da4b4624
Mishin Yura <mishin.inbox@gmail.com>
2022-09-05 09:09:59 +0300

    Bootstrap the project. Closes #1

---
+++ src/test/java/com/mishinyura/bookstesting/AppTests.java
@@ -0,0 +1,13 @@
+package com.mishinyura.bookstesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class AppTests {
+    @Test
+    void contextLoads() {
+    }
+}
